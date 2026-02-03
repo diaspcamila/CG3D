c0d1aaab58a23a32037c704228aa3fc059ecb1d1
diaspcamila <diascamila2005@gmail.com>
2026-02-03 17:58:30 -0300

    add var camera e iluminacao

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var angle = 0
+
 function getGL(canvas)
 {
     var gl = canvas.getContext("webgl");
@@ -110,4 +112,84 @@ function init()
     gl.clear( gl.COLOR_BUFFER_BIT);
 
     gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    var normals = new Float32Array([
+        //Quad 1
+        					0, 0, 1,
+        					0, 0, 1,
+        					0, 0, 1,
+        					0, 0, 1,
+        					0, 0, 1,
+        					
+        					//Quad 2
+        					1, 0, 0,
+        					1, 0, 0,
+        					1, 0, 0,
+        					1, 0, 0,
+        					1, 0, 0,
+        					
+        					//Quad 3
+        					0, 1, 0,
+        					0, 1, 0,
+        					0, 1, 0,
+        					0, 1, 0,
+        					0, 1, 0
+    ]);
+
+    //Cria buffer na GPU e copia coordenadas para ele
+    var bufnormalPtr = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufnormalPtr);
+    gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+        
+    //Pega ponteiro para o atributo "position" do vertex shader
+    var normalPtr = gl.getAttribLocation(prog, "normal");
+    gl.enableVertexAttribArray(normalPtr);
+    //Especifica a cópia dos valores do buffer para o atributo
+    gl.vertexAttribPointer(normalPtr, 
+      					   3,        //quantidade de dados em cada processamento
+       					   gl.FLOAT, //tipo de cada dado (tamanho)
+       					   false,    //não normalizar
+       					   0,      //tamanho do bloco de dados a processar em cada passo
+       					             //0 indica que o tamanho do bloco é igual a tamanho
+       					             //lido (2 floats, ou seja, 2*4 bytes = 8 bytes)
+       					   0         //salto inicial (em bytes)
+       					  );
+        					  
+    var lightDirectionPtr = gl.getUniformLocation(prog, "lightDirection");
+    gl.uniform3fv(lightDirectionPtr, [0, 0, -1]/*[-0.2, -1, -0.7]*/);
+        
+    var lightColorPtr = gl.getUniformLocation(prog, "lightColor");
+    gl.uniform3fv(lightColorPtr, [1, 1, 1]);
+        
+    var lightposPtr = gl.getUniformLocation(prog, "lightpos");
+    gl.uniform3fv(lightposPtr, [0.25, 0.0, 0.5]);
+        
+    var camposPtr = gl.getUniformLocation(prog, "campos");
+    gl.uniform3fv(camposPtr, campos);
+}
+
+function createCamera(pos, target, up){
+    var zc = math.subtract(pos, target);
+    zc = math.divide(zc, math.norm(zc));
+
+    var yt = math.subtract(up, pos);
+    yt = math.divide(yt, math.norm(yt));
+
+    var xc = math.cross(yt, zc);
+    xc = math.divide(yc, math.norm(yc));
+
+    var mt = math.inv(math.transpose(math.matrix([xc, yc, zc])));
+
+    mt = math.resize(mt, [4,4], 0);
+    mt._data[3][3] = 1;
+
+    var mov = math.matrix([[ 1, 0, 0, -pos[0]],
+        [0, 1, 0, -pos[1]],
+        [0, 0, 1, -pos[2]],
+        [0, 0, 0, 1]
+    ]);
+
+    var cam = math.multiply(mt, mov);
+
+    return cam;
 }
