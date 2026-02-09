a434b10f50f2b001cbec31165d881e0b5ca229b3
diaspcamila <diascamila2005@gmail.com>
2026-02-09 01:43:47 -0300

    unused functions

--- transforms.js
+++ transforms.js
@@ -14,24 +14,6 @@ function matrotZ(angle){
     ]);
 }
 
-function matrotY(angle){
-    return math.matrix([
-        [ math.cos(angle*Math.PI/180),0,math.sin(angle*Math.PI/180),0],
-        [0,1,0,0],
-        [-math.sin(angle*Math.PI/180),0,math.cos(angle*Math.PI/180),0],
-        [0,0,0,1]
-    ]);
-}
-
-function matrotX(angle){
-    return math.matrix([
-        [1,0,0,0],
-        [0,math.cos(angle*Math.PI/180),-math.sin(angle*Math.PI/180),0],
-        [0,math.sin(angle*Math.PI/180), math.cos(angle*Math.PI/180),0],
-        [0,0,0,1]
-    ]);
-}
-
 
 function mattrans(tx, ty, tz){
     return math.matrix([
@@ -40,29 +22,4 @@ function mattrans(tx, ty, tz){
         [0, 0, 1, tz],
         [0, 0, 0, 1]
     ]);
-}
-
-function matescala(sx, sy, sz){
-    return math.matrix([
-        [sx, 0, 0, 0],
-        [0, sy, 0, 0],
-        [0, 0, sz, 0],
-        [0, 0, 0, 1]
-    ]);
-}
-
-function normalMat(modelMatrix){
-    const inv = math.inv(modelMatrix);
-
-    const trans = math.transpose(inv);
-
-    return trans;
-}
-
-function sendMat(gl, uniformLocation, matrixMathJs){
-    const flatArray = matrixMathJs.toArray().flat();
-
-    const data = new Float32Array(flatArray);
-
-    gl.uniformMatrix4fv(uniformLocation, true, data);
 }
